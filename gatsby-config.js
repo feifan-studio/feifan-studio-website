37d1853d4f0172a3abb68adfed81b0a38eec6c17
Drew Wei <drewwei0923@gmail.com>
2022-10-05 00:03:41 -0500

    Reorganized with our own photos.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,4 @@
-require(`dotenv`).config()
+require('dotenv').config()
 
 const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
 
@@ -10,65 +10,64 @@ module.exports = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-jodie/gatsby-config.js
-    siteTitle: `Jodie`,
-    siteTitleAlt: `Jodie - Gatsby Starter Portfolio`,
-    siteHeadline: `Jodie - Gatsby Theme from @lekoarts`,
-    siteUrl: `https://jodie.lekoarts.de`,
-    siteDescription: `Image-heavy photography portfolio with colorful accents & customizable pages. Includes adaptive image grids powered by CSS grid and automatic image integration into projects.`,
-    siteImage: `/banner.jpg`,
-    author: `@lekoarts_de`,
+    siteTitle: 'Feifan Studio',
+    siteTitleAlt: 'Feifan Studio',
+    siteHeadline: 'Feifan Studio @ Austin, TX',
+    siteUrl: 'https://feifan-studio.netlify.app',
+    siteDescription: 'Feifan Studio @ Austin, TX.',
+    siteImage: '/banner.jpg',
+    author: '@feifan.studio'
   },
   plugins: [
     {
-      resolve: `@lekoarts/gatsby-theme-jodie`,
+      resolve: '@lekoarts/gatsby-theme-jodie',
       // See the theme's README for all available options
       options: {
         navigation: [
-          { name: `Projects`, slug: `/projects` },
-          { name: `Art`, slug: `/art` },
-          { name: `About`, slug: `/about` },
-        ],
-      },
+          { name: 'About', slug: '/about' },
+          { name: 'Projects', slug: '/projects' }
+        ]
+      }
     },
     {
-      resolve: `gatsby-plugin-sitemap`,
+      resolve: 'gatsby-plugin-sitemap',
       options: {
-        output: `/`,
-      },
+        output: '/'
+      }
     },
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        name: `jodie - @lekoarts/gatsby-theme-jodie`,
-        short_name: `jodie`,
-        description: `Image-heavy photography portfolio with colorful accents & customizable pages. Includes adaptive image grids powered by CSS grid and automatic image integration into projects.`,
-        start_url: `/`,
-        background_color: `#ffffff`,
+        name: 'Feifan Studio',
+        short_name: 'Feifan',
+        description: 'Feifan Studio @ Austin, TX.',
+        start_url: '/',
+        background_color: '#ffffff',
         // This will impact how browsers show your PWA/website
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#b75e09`,
-        display: `standalone`,
+        display: 'standalone',
         icons: [
           {
-            src: `/android-chrome-192x192.png`,
-            sizes: `192x192`,
-            type: `image/png`,
+            src: '/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
           },
           {
-            src: `/android-chrome-512x512.png`,
-            sizes: `512x512`,
-            type: `image/png`,
-          },
-        ],
-      },
+            src: '/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
     },
     shouldAnalyseBundle && {
-      resolve: `gatsby-plugin-webpack-bundle-analyser-v2`,
+      resolve: 'gatsby-plugin-webpack-bundle-analyser-v2',
       options: {
-        analyzerMode: `static`,
-        reportFilename: `_bundle.html`,
-        openAnalyzer: false,
-      },
-    },
-  ].filter(Boolean),
+        analyzerMode: 'static',
+        reportFilename: '_bundle.html',
+        openAnalyzer: false
+      }
+    }
+  ].filter(Boolean)
 }
